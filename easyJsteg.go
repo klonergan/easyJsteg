27cb3e17d59bf22a89581b678a86309943620aa6
klonergan <kevmlonergan@gmail.com>
2020-08-16 12:30:30 -0700

    ignore compiled executable

--- easyJsteg.go
+++ easyJsteg.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"./steg"
 )
 
 func main() {
-	steg.Encode(os.Args[1], os.Args[2], os.Args[3])
+	err := steg.Encode(os.Args[1], os.Args[2], os.Args[3])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
