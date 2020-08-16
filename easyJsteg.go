d05f6e18afd37cd7f5fad3d28014e4d6bb3601b1
klonergan <kevmlonergan@gmail.com>
2020-08-16 15:21:15 -0700

    allow encoding of file data

--- easyJsteg.go
+++ easyJsteg.go
@@ -12,12 +12,13 @@ import (
 func main() {
 	// define flags
 	var d, e bool
-	var i, o, m string
+	var i, o, m, f string
 	flag.BoolVar(&d, "d", false, "decode mode")
 	flag.BoolVar(&e, "e", false, "encode mode")
-	flag.StringVar(&i, "i", "", "define input filename for encode or decode")
+	flag.StringVar(&i, "i", "", "define input jpg filename for encode or decode")
 	flag.StringVar(&o, "o", "output.jpg", "define output filename for encode")
 	flag.StringVar(&m, "m", "", "use to include a message as a string")
+	flag.StringVar(&f, "f", "", "declare a filename of a file to be hidden")
 	flag.Parse()
 	if (d == true && e == true) || (d == false && e == false) {
 		fmt.Println("use -d to decode OR -e to encode a message")
@@ -25,10 +26,18 @@ func main() {
 	}
 	// encode mode
 	if e == true {
-		err := steg.Encode(i, o, m)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
+		if f == "" {
+			err := steg.Encode(i, o, m)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(0)
+			}
+		} else {
+			err := steg.EncodeFile(i, o, f)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(0)
+			}
 		}
 	}
 	// decode mode
--- steg/encode.go
+++ steg/encode.go
@@ -3,6 +3,7 @@ package steg
 import (
 	"fmt"
 	"image/jpeg"
+	"io/ioutil"
 	"os"
 
 	"lukechampine.com/jsteg"
@@ -11,7 +12,6 @@ import (
 // Encode a message in an image
 func Encode(inputFilename, outputFilename, encodedString string) error {
 	// open a jpeg
-	// inputFilename := os.Args[1]
 	f, err := os.Open(inputFilename)
 	if err != nil {
 		return err
@@ -22,14 +22,45 @@ func Encode(inputFilename, outputFilename, encodedString string) error {
 	}
 
 	//add hidden data to jpeg
-	// outputFilename := os.Args[2]
 	out, err := os.Create(outputFilename)
 	if err != nil {
 		return err
 	}
-	// encodedString := os.Args[3]
 	encodedStringLen := len(encodedString)
-	data := []byte(fmt.Sprint(encodedStringLen) + ":" + encodedString)
+	// the hidden data is formated length:type/data. the type is a file name or an m for message
+	data := []byte(fmt.Sprint(encodedStringLen) + ":" + "m/" + encodedString)
+	hideErr := jsteg.Hide(out, img, data, nil)
+	if hideErr != nil {
+		return hideErr
+	}
+	return nil
+}
+
+// EncodeFile encodes a file in an image
+func EncodeFile(inputFilename, outputFilename, messageFilename string) error {
+	// open a jpeg
+	f, err := os.Open(inputFilename)
+	if err != nil {
+		return err
+	}
+	img, err := jpeg.Decode(f)
+	if err != nil {
+		return err
+	}
+	// open the file to be hidden
+	h, err := ioutil.ReadFile(messageFilename)
+	if err != nil {
+		return err
+	}
+	encodedData := string(h)
+	encodedDataLen := len(encodedData)
+	// add hidden data to jpeg
+	out, err := os.Create(outputFilename)
+	if err != nil {
+		return err
+	}
+
+	data := []byte(fmt.Sprint(encodedDataLen) + ":" + messageFilename + "/" + encodedData)
 	hideErr := jsteg.Hide(out, img, data, nil)
 	if hideErr != nil {
 		return hideErr
