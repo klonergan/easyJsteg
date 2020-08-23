0d0cc5689e8f253e71b3cc0ddcc2d9e8b12b66e7
klonergan <kevmlonergan@gmail.com>
2020-08-23 16:23:48 -0700

    get file and message from html form

--- web/server.go
+++ web/server.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func testRoute(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "test route\n")
+	if req.Method == "POST" {
+		var buf bytes.Buffer
+		err := req.ParseMultipartForm(1 << 25)
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
+		fmt.Println(req.Header.Get("Content-Type"))
+		file, header, err := req.FormFile("img")
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
+		defer file.Close()
+		name := header.Filename
+		fmt.Println(name)
+		io.Copy(&buf, file)
+		fmt.Println("buf: ", buf.String())
+		m := req.FormValue("m")
+		fmt.Println(m)
+		// body, err := ioutil.ReadAll(req.Body)
+		// if err != nil {
+		// 	fmt.Println(err)
+		// }
+		// fmt.Println(string(body))
+	} else {
+		fmt.Fprintf(w, "test route\n")
+		fmt.Println(*req)
+	}
 }
 
 func main() {
